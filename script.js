4123a26edefd332353a6f95300ec7015b156f202
colly1 <165697868+colly1@users.noreply.github.com>
2025-08-06 13:24:12 -0300

    salva

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function  inserirNome(){
-    let nomeUsario= prompt("Qual o seu nome");
-    let elemente = document.querySelector("#nome-")
+function inserirNome(){
+let nomeUsuario = prompt("Qual oseu nome");
+    let elemento = document.querySelector("#nome-usuario");
+    console.log(elemento);
+    elemento.textContent = nomeUsuario;
 }
+
+const lista = document.querySelector('#lista')
+lista.textContent = linguagens[0];
+lista.textContent = linguagens[1];
+lista.textContent = linguagens[2];
+
+let aluno1 ={
+    nome: 'Carlos',
+    idade: 17,
+    anoletivo: 'Ensino Médio',
+    matériasFavoritas: ['Matemática, Português, Física']
+}
+
+console.log(aluno1.nome);
+console.log(aluno1.idade);
+console.log(aluno1.materisFavoritas);
+console.log(aluno1.matériasFavoritas[1]);
+
+const objeto = document.querySelector("#aluno");
+objeto.textContent = aluno1.nome;
+objeto.textContent = aluno1.idade;
+objeto.textContent = aluno1.materiasFavoritas[1];
